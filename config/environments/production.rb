9259580f80d8606d051577468d5e5c7e7cc9b5d8
Carlos <carlos8390@hotmail.com>
2017-03-07 11:59:46 -0500

    Rename Local Variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,9 +89,9 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               ENV[“GMAIL_DOMAIN”],
-  user_name:            ENV[“GMAIL_USERNAME”],
-  password:             ENV[“GMAIL_PASSWORD”],
+  domain:               ENV['GMAIL_DOMAIN'],
+  user_name:            ENV['GMAIL_USERNAME'],
+  password:             ENV['GMAIL_PASSWORD'],
   authentication:       'login',
   enable_starttls_auto: true  }
 
