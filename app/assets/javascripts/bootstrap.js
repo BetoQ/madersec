1c56e07c18663def9ae4a63b4763cb2a757e04f2
Carlos <carlos8390@hotmail.com>
2017-03-11 14:04:29 -0500

    end end18

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -2375,3 +2375,18 @@ if (typeof jQuery === 'undefined') {
   })
 }(jQuery);
 
+$(document).ready(function(){       
+                var scroll_start = 0;
+                var startchange = $('#startchange');
+                var offset = startchange.offset();
+                    if (startchange.length){
+                $(document).scroll(function() { 
+                    scroll_start = $(this).scrollTop();
+                    if(scroll_start > offset.top) {
+                          $(".navbar-default").css('background-color', '#ffffff');
+                       } else {
+                          $('.navbar-default').css('background-color', 'transparent');
+                       }
+                   });
+                    }
+                });
